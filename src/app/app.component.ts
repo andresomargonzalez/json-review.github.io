b547dd3784573a36c83eac4d395887a0e2f27763
omar <omar@intelifaz.com>
2022-02-21 12:02:31 -0300

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'website';
+  displayedColumns: string[] = ['description', 'brandName', 'foodCategory', 'marketCountry', 'packageWeight', 'servingSize'];
+  dataSource: any = [];
+  value: string = 'Cheddar Cheese';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  async ngOnInit() {
+    const { foods }: any  = await this.http.get(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=${this.value}`).toPromise();
+    this.dataSource = foods
+  }
+
+  async onSearch() {
+    const { foods }: any  = await this.http.get(`https://api.nal.usda.gov/fdc/v1/foods/search?api_key=DEMO_KEY&query=${this.value}`).toPromise();
+    this.dataSource = foods
+    console.log(foods);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatTableModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
